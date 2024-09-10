82aca74b97190dff860ce5b08d0ffb975ad6c2b2
Luiz Carlos <luizcmoficial@gmail.com>
2024-09-10 15:48:20 -0300

    for deploy

--- modelo/conexao.js
+++ modelo/conexao.js
@@ -4,9 +4,20 @@ const options = {
     useNewUrlParser: true
 }
 
-banco
-    .connect("mongodb://localhost:27017/livraria", options)
-    .then(() => console.log("Conectado ao MongoDB!"))
-    .catch((error) => console.log(`Erro: ${error}}`));
+// COMO O TRABALHO SOLICITOU
+// banco
+//     .connect("mongodb://localhost:27017/livraria", options)
+//     .then(() => console.log("Conectado ao MongoDB!"))
+//     .catch((error) => console.log(`Erro: ${error}}`));
+
+
+// PARA DEPLOY
+const conectarBanco = async () => {
+    await banco.connect("mongodb://localhost:27017/livraria", options)
+    .then(() => {console.log("Conectado ao MongoDB!");})
+    .catch((error) => console.log(`Erro: ${error}}`));;
+}
+
+conectarBanco();
 
 module.exports = banco;
